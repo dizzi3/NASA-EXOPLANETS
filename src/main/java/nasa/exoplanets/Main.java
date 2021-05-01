06aa91575f6f4f1a30f6b25a2779f13ac8cab114
Michael <michas2820@onet.eu>
2021-05-01 18:41:11 +0200

    Initial commit

---
+++ src/main/java/nasa/exoplanets/Main.java
@@ -0,0 +1,34 @@
+package nasa.exoplanets;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);	
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(this.getClass().getResource("/fxml/MainScreen.fxml"));
+		
+		StackPane stackPane = loader.load();
+		
+		Scene scene = new Scene(stackPane);
+		
+		//TODO: implement styles
+		//scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("NASA EXOPLANETS");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+	
+}
